5eaad55ad4553d7c145d50996883110822056fe3
Sivaragul <111353868+Sivaragul@users.noreply.github.com>
2022-08-24 21:25:38 +0530

    Create 24aug.program.cs

---
+++ 24August/24aug.program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResuableCodeLibrary;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SampleClass.PrintString();
+            problem1.Twostring();
+            problem2.Copy();
+            problem3.Index();
+            problem4.split();
+            problem5.Uppercase();
+            problem6.filename();
+            problem7.Day();
+            Console.ReadLine();
+        }
+    }
+}
